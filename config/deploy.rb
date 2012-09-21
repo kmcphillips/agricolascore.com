7dddce83a9ab9ef1d384a309729a074607e9243c
Kevin McPhillips <github@kevinmcphillips.ca>
2012-09-20 19:29:58 -0500

    Updated favicon and apple touch icon. Capify.

---
+++ config/deploy.rb
@@ -0,0 +1,38 @@
+## Deployment config for production
+set :application, "Agricola Score"
+set :deploy_to, "/var/www/kevin/data/www/agricolascore.com"
+set :user, "kevin"
+set :use_sudo, false
+set :keep_releases, 5
+
+## Configure source control
+set :scm, "git"
+set :repository,  "git://github.com/kimos/agricolascore.com.git"
+set :branch, "master"
+set :deploy_via, :checkout
+set :git_shallow_clone, 1
+
+## Fix for requiring terminal on prod server
+default_run_options[:pty] = true
+
+## Role for production
+role :web, "68.169.58.121"
+role :app, "68.169.58.121"
+
+## Tasks for deploying to Apache Passenger
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+## install gems through bundler
+namespace :bundle do
+  task :install do
+    run "cd #{current_path} && bundle install"
+  end
+end
+
+after "deploy:update", "deploy:cleanup"
